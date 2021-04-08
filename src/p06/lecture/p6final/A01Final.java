f4d8dcd5c27b964640cb13bef6f4ada1cc94668a
id91y <id91y@naver.com>
2021-04-08 15:50:57 +0900

    java

---
+++ src/p06/lecture/p6final/A01Final.java
@@ -0,0 +1,5 @@
+package p06.lecture.p6final;
+
+public class A01Final {
+
+}
--- src/p99/codingbat/WarmUp1Solution.java
+++ src/p99/codingbat/WarmUp1Solution.java
@@ -1,6 +1,93 @@
 package p99.codingbat;
 
 public class WarmUp1Solution {
+	public String missingChar(String str, int n) {
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			if (i != n) {
+				res += str.charAt(i);
+			}
+		}
+
+		return res;
+	}
+
+	public String notString(String str) {
+		if (str.startsWith("not")) {
+			return str;
+		} else {
+			return "not " + str;
+		}
+
+		/*
+		 * if (str.length() < 3) { return "not " + str; }
+		 * 
+		 * String front = str.substring(0, 3);
+		 * 
+		 * if (front.equals("not")) { return str; } else { return "not " + str; }
+		 */
+	}
+
+	public boolean posNeg(int a, int b, boolean negative) {
+		if (negative) {
+			return (a < 0) && (b < 0);
+		} else {
+			return (a * b) < 0;
+		}
+	}
+
+	public boolean nearHundred(int n) {
+		int diff = n - 100;
+
+		if (diff >= -10 && diff <= 10) {
+			return true;
+		}
+
+		diff = n - 200;
+
+		if (diff >= -10 && diff <= 10) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean makes10(int a, int b) {
+		if (a == 10 || b == 10) {
+			return true;
+		}
+
+		if (a + b == 10) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	public boolean parrotTrouble(boolean talking, int hour) {
+
+		if (talking) {
+			if (hour < 7 || hour > 20) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int diff21(int n) {
+		if (n < 22) {
+			return 21 - n;
+		} else {
+			return (n - 21) * 2;
+		}
+	}
+
+	public int sumDouble(int a, int b) {
+		return (a == b) ? (a + b) * 2 : (a + b);
+	}
+
 	public boolean in1020(int a, int b) {
 		// return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
 
